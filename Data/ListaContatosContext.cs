1c3362d213c031ebc78e2e7c201a372e30df42bc
Adriana Borges <drika.fonseca.borges@gmail.com>
2022-04-20 01:30:50 -0300

    + Criação da estrutura para persistir os contatos;

---
+++ Data/ListaContatosContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ListaContatos.Models;
+
+namespace ListaContatos.Data
+{
+    public class ListaContatosContext : DbContext
+    {
+        public ListaContatosContext (DbContextOptions<ListaContatosContext> options)
+            : base(options)
+        {
+            // Recria o banco sempre que inicializado
+            Database.EnsureCreated();
+
+        }
+
+        public DbSet<ListaContatos.Models.Contatos> Contatos { get; set; }
+    }
+}
---
+++ Models/Contatos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaContatos.Models
+{
+    public class Contatos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+    }
+}
