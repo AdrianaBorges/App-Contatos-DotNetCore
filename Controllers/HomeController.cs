00a8b2c467a222917e37d26d5767753df31643e5
Adriana Borges <drika.fonseca.borges@gmail.com>
2022-04-26 00:17:06 -0300

    + Inclusão da tela de contatos na Home;

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,8 @@ namespace ListaContatos.Controllers
             return View();
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
